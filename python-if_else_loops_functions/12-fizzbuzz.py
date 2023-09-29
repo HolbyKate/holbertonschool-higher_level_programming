0b52f0d3586faf89181fc5f668c435ac7d980607
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-09-29 13:06:09 -0700

    fix first if with 15

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 for i in range(1, 101):
-    if i % 3 == 0 and i % 5 == 0:
-        print("FizzBuzz", end="")
+    if i % 15 == 0:
+        print("FizzBuzz ", end="")
     elif i % 3 == 0:
-        print("Fizz", end="")
+        print("Fizz ", end="")
     elif i % 5 == 0:
-        print("Buzz", end="")
+        print("Buzz ", end="")
     else:
-        print("{:d}".format(i), end="")
-    print(" ", end="")
+        print("{}".format(i), end="")
