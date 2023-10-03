423462b4392fb751f3d60d43b1be81158d678665
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-03 01:31:03 -0700

    Task 4 replace in a copy

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list)
+    list_new = my_list.copy()
+    if idx < 0 or idx > length - 1:
+        return(list_new)
+    list_new[idx] = element
+    return (list_new)
