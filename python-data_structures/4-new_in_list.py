3e61d9cfa74e90b476bd702df969a0428374f205
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-03 01:34:36 -0700

    Fix : change order of variable

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    length = len(my_list)
     list_new = my_list.copy()
+    length = len(my_list)
+
     if idx < 0 or idx > length - 1:
-        return(list_new)
+        return (list_new)
     list_new[idx] = element
     return (list_new)
