aa4931b09da54af994bda77d7b99879e11f0aa43
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-03 03:59:24 -0700

    Fix initialize varaiable

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 def no_c(my_string):
     new_string = ""
+    if my_string is None:
+        return
     for char in my_string:
         if char != "c" and char != "C":
             new_string += char
