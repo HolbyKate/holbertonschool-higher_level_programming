fff7e0db61005b1076de9e3ae1c7add32082b893
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-03 05:59:41 -0700

    Fix raw and column

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,9 @@
 def print_matrix_integer(matrix=[[]]):
     if (matrix is None):
         return
-    for i in (matrix):
-        print("{}".format(i))
+    for raw in matrix:
+        for column in raw:
+            print("{:d}".format(column), end="")
+            if column != raw [-1]:
+                print(" ", end="")
+        print()
