ea53f472bb8017ccc6d54654c5dffd594f652a4a
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-04 00:47:01 -0700

    Fix: my_list == 0

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (my_list is None):
+    if len(my_list) == 0:
         return (None)
-
     sorted_list = sorted(my_list)
     return (sorted_list[-1])
