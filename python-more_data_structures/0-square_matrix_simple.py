0a6d600db70122016efcdd35a80fe1c75f85be85
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-04 06:41:03 -0700

    Fix: requirement

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     mapped_matrix = [list(map(lambda x: x ** 2, raw)) for raw in matrix]
-    return (mapped_matrix)
+    return mapped_matrix
