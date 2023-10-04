c818d8a32879d68870e21a335e665e6a75596a88
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-04 00:34:06 -0700

    Fix : None

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == "":
+    if (my_list is None):
         return (None)
+
     sorted_list = sorted(my_list)
     return (sorted_list[-1])
