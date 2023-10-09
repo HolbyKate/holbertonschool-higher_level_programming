ba597ea00baee5b29d250f11bdd74c238e3c073a
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-09 05:54:32 -0700

    Instance method that returns current square area

---
+++ python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private instance
+"""
+
+
+class Square:
+    """Write a empty class"""
+    def __init__(self, size=0):
+        self.__size = size
+        """define private instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """define square"""
+    
+    def area(self):
+        """define instance method that returns the current square area"""
+        return self.__size ** 2
