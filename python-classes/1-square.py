5f12be66c04425f51321391cc3913cceda524600
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-09 03:15:07 -0700

    Define private instance attribute to size

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 1-square
+Defines class Square with private instance
+"""
+
+
+class Square:
+    """Write a empty class"""
+    def __init__(self, size):
+        self.__size = size
+        """define private instance attribute"""
