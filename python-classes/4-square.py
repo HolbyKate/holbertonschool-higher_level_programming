946e4745cab4735abd90e30a9defbef6297f741f
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-09 07:08:59 -0700

    Return the current square area with property

---
+++ python-classes/4-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module 4-square
+Defines class Square based on 3-square.py
+"""
+
+
+class Square:
+    """Write a empty class"""
+    def __init__(self, size=0):
+        self.__size = size
+        """define private instance attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        """define square"""
+
+    def area(self):
+        """define instance method that returns the current square area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieve the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square with error checking"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
