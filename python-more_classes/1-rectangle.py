812823a7cb7d754e9ae6309b9faf1727dd144f9f
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-11 04:48:40 -0700

    Fix loops elif else

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,40 +1,43 @@
 #!/usr/bin/python3
-"""Write an empty class Rectangle that defines a rectangle"""
-"""based on 0-rectangle.py"""
+"""
+Write an empty class Rectangle that defines a rectangle
+"""
 
 
 class Rectangle:
-    """Write a empty class"""
+    """Define a rectangle"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
-        @property
-        def width(self):
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            if not isinstance(value, int):
-                raise TypeError("width must be an integer")
-            if value < 0:
-                raise ValueError("width must be >= 0")
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
             self.__width = value
 
-        @property
-        def height(self):
-            return self.__height
+    @property
+    def height(self):
+        return self.__height
 
-        @height.setter
-        def height(self, value):
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
             self.__height = value
 
-        def area(self):
-            return self.width * self.height
+    def area(self):
+        return self.width * self.height
 
-        def perimeter(self):
-            return 2 * (self.width + self.height)
+    def perimeter(self):
+        return 2 * (self.width + self.height)
