43d83557528f561266de34e339d37a277eed2538
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-11 05:25:25 -0700

    Fix: self._width and self.height

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -7,8 +7,8 @@ Write an empty class Rectangle that defines a rectangle
 class Rectangle:
     """Define a rectangle"""
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
