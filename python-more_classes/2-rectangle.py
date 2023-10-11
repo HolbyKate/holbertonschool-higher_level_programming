ebc803f7cc25f7bb371e488d03044513e44adbb3
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-11 05:58:51 -0700

    Fix order of instance method

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -45,4 +45,4 @@ class Rectangle:
         if self.__width or self.__height == 0:
             return 0
         else:
-            return ((self.__width + self.__height) * 2)
+            return 2 * (self.__width + self.__height)
