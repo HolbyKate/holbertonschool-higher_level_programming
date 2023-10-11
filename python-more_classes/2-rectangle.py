403944f6e4250fb0d12695d8a1b2e2e4bdf227e1
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-11 06:08:45 -0700

    Fix: width == O

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -42,7 +42,8 @@ class Rectangle:
         return self.__width * self.__height
 
     def perimeter(self):
-        if self.__width or self.__height == 0:
+        """ if width or height is equal to 0, perimeter is equal to 0"""
+        if self.__width == 0 or self.__height == 0:
             return 0
         else:
-            return 2 * (self.__width + self.__height)
+            return (self.__width + self.__height) * 2
