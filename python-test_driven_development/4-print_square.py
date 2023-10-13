ee273fd720db1d349aefc1841fcfa1928e9939b2
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-13 12:06:23 -0700

    Function that prints a square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This module contains a function that prints a square"""
+
+
+def print_square(size):
+    """This function prints a square with the character #"""
+
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for _ in range(size):
+        print("#" * size)
