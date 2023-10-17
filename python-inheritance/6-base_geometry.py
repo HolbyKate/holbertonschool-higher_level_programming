58f2bb8f2c5b5b52f2474704e0f09abea2057333
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-17 13:07:15 -0700

    Raise exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Task: 6-base_geometry.py"""
+
+
+class BaseGeometry():
+    """Create an empty class"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
