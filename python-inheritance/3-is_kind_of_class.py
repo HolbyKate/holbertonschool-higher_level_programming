e8b7f2f78c88ccda4dad6c313025cd3352d9c54f
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-17 06:19:50 -0700

    If type is an obj or isintance of a class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Task: 3-is_kind_of_class.py"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    Returns True if the object is an instance of, or if the object is an
+    instance of a class that inherited from, the specified class ;
+    otherwise False.
+    """
+    if type(obj) == a_class or isinstance(obj, a_class):
+        return True
+    else:
+        return False
