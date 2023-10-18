217bb054d80c4fa2eb04f3d6f2659128d1e3dfe6
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-18 01:18:23 -0700

    Fix __str__

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,18 +11,18 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         """Instantiation with width and height"""
         self.integer_validator("width", width)
-        self._width = width
+        self.__width = width
         """Define positive integers, validated by integer_validator"""
         self.integer_validator("height", height)
-        self._height = height
+        self.__height = height
 
     def area(self):
         """Return the area of the rectangle"""
-        return self._width * self._height
+        return self.__width * self.__height
 
-    def _str_(self):
+    def __str__(self):
         """
         print() should print, and str() should return, the following rectangle
         description: [Rectangle] <width>/<height>
         """
-        return f"[Rectangle] {self._width}/{self._height}"
+        return (f"[Rectangle] {self.__width}/{self.__height}")
