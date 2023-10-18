aa66c7b2a19c87311037a53eeac2294aedb2c272
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-18 13:46:11 -0700

    write file and return the number

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task: 1-write_file.py"""
+
+
+def write_file(filename="", text=""):
+    """
+    Function that writes a string to a text file (UTF8)
+    and returns the number of characters written:
+    """
+    with open(filename, "w", encoding="utf-8") as my_file:
+        my_file.write(text)
+        return len(text)
