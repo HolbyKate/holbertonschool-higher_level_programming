c95de73c27c94dfda9f90d0641ee492de6f3e2cc
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-19 01:52:09 -0700

    Write an Object to a text file using JSON

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -11,4 +11,4 @@ def save_to_json_file(my_obj, filename):
     using a JSON representatio
     """
     with open(filename, "w", encoding="utf-8") as f:
-        return json.dumps(my_obj)
+        return json.dump(my_obj, f)
