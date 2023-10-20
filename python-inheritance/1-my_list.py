200f8d2b0394df3a25ae635108766b25200fb919
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-20 10:41:39 +0200

    Fix class Mylist

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,7 +2,10 @@
 """Task: 1-my_list"""
 
 
+class Mylist(list):
+    """Write a class MyList that inherits from list"""
+
+
 def print_sorted(self):
     """Write a class MyList that inherits from list"""
     print(sorted(self))
-    
