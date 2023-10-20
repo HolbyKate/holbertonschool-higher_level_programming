a487c0b62892de21447945b62c44518331490f93
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-20 10:16:27 +0200

    Fix PEP8

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -4,12 +4,12 @@
 
 class Student():
     """class Student that defines a student by: (based on 9-student.py)"""
-    
+
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a Student instance"""
         if attrs is None:
