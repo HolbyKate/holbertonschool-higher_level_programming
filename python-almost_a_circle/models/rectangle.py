d48ed55d2cef907b8778e9084429c2e93850eb3a
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-10-23 14:39:53 -0700

    Adding **kwargs as a "key-worded argument”

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -111,7 +111,7 @@ class Rectangle(Base):
         h1 = self.__height
         return (f"[{rec}] ({id1}) {x1}/{y1} - {w1}/{h1}")
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """Assign an argument to each attribute"""
         if args and len(args) != 0:
             a = 0
@@ -130,3 +130,19 @@ class Rectangle(Base):
                 elif a == 4:
                     self.y = arg
                 a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
