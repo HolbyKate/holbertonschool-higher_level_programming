ea4b3434f3038dfde37c8d5b82082d80015a26b0
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-22 06:40:23 -0800

    Print description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- print a drescription of the table first_table from database hbtn_0c_0
+SELECT * FROM hbtn_0c_0.first_table;
+Table   Create Table                                                                         
+first_table     CREATE TABLE `first_table` (\n  `id` int DEFAULT NULL,\n  `name` varchar(256) DEFAULT NULL\n) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci 
