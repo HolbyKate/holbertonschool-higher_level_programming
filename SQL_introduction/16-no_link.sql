3ec49fc63b8798dcc0a6911f507af009a9e394b6
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-23 02:27:33 -0800

    fix order

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table second_table of the database
-SELECT * FROM second_table ORDER BY number ASC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC, name DESC;
