266e9e3e3c05129b9a4b139a316b68dfff59ca83
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-23 01:49:32 -0800

    Fix average

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,4 +1,2 @@
--- computes the score average of all records in the table second_table of the database hbtn_0c_0
-CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
-USE hbtn_0c_0;
+-- computes the score average of all records in the table second_table of the database 
 SELECT AVG(score) FROM second_table;
