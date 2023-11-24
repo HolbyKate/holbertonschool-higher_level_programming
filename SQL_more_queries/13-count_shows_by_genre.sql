ee28d4f41595b0ae2acc85fa17510452ee1098b5
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-24 06:11:43 -0800

    Fix having count and DESC

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,7 @@
 --  lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT genres.name, COUNT(tv_shows.id) AS count
-FROM genres
-LEFT JOIN tv_show_genres
-ON genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
-ORDER BY count DESC, genres.name ASC;
+HAVING COUNT(tv_shows_genres.show_id) > 0
+ORDER BY count DESC;
