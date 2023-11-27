a988348c18b29c9cba6b66b9e14abae3f2f2dd00
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-27 05:57:03 -0800

    Fix binary name

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,7 +18,8 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute(
+        "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
 
     for row in cursor.fetchall():
         print(row)
