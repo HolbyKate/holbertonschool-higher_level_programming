8e8cedf6c1ab854812ff322090658c2e9c764af5
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-27 05:23:59 -0800

    Fix: chmod

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""lists all states from the database hbtn_0e_0_usa"""
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 import sys
 
@@ -15,7 +18,7 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE "N%"")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
 
     for row in cursor.fetchall():
         print(row)
