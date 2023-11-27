e160c2a8af6ffb6a11d1ff6d753d0fdba7a6c8df
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-27 12:38:18 -0800

    Update cursor.execute

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -15,9 +15,10 @@ if __name__ == "__main__":
     )
 
     cursor = database.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities\
-        INNER JOIN states ON cities.state_id = states.id\
-        ORDER BY cities.id ASC") 
+    cursor.execute("""
+                   SELECT cities.id, cities.name, states.name FROM cities
+                   INNER JOIN states ON cities.state_id = states.id
+                   ORDER BY cities.id ASC""") 
 
     for row in cursor.fetchall():
         print(row)
