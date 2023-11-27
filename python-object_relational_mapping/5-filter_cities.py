6026abae2c692375fc1ccf9b6bbae24a29535154
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-27 12:50:22 -0800

    Test

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -14,16 +14,18 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         password=sys.argv[2],
-        database=sys.argv[3]
+        database=sys.argv[3],
+        state_name=sys.argv[4]
     )
 
     cursor = database.cursor()
-    cursor.execute("""
-                   SELECT cities.name FROM cities
-                   INNER JOIN states ON cities.state_id = states.id
-                   WHERE states.name = %s
-                   ORDER BY cities.id ASC
-                   """, (sys.argv[4],))
+    query = """
+            SELECT cities.name FROM cities
+            INNER JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
+            """
+    cursor.execute(query, (state_name,))
 
     for row in cursor.fetchall():
         print(row)
