4af3487a30edfa811790383797b3d6ed6285dc8a
HolbyKate <cathy.augustin@holbertonstudents.com>
2023-11-28 02:15:28 -0800

    Fix for checker forgot changes

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that prints all City objects from the database hbtn_0e_14_usa.
+Script that prints all City objects from the database hbtn_0e_14_usa
 """
 
 import sys
